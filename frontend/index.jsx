b8d9cba2d30f4db69eef8aff3600556722124ff8
Aleksei Naumov <naumov78@gmail.com>
2017-04-16 23:52:20 -0400

    change config

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import App from './components/app';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
